1efed0c6bf3944dce8bee12394ef2e36b62db007
Leghuir <qwerty1214@gmail.com>
2017-10-26 14:02:06 +0200

    genetic algorithm full

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace ConsoleApplication29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime D = DateTime.Now;
+            List<int> srednia = new List<int>();
+            StreamWriter sw = File.AppendText("Wyniki.txt");
+            for (int y=0;y<10;y++)
+            { 
+            
+            Analiza A = new Analiza(40);
+            
+            Random szansa_krzyzowania = new Random();
+            for (int i = 0; i < 200000; i++)
+            {
+                    A.krzyzowanie_pmx(szansa_krzyzowania.Next(100));
+                    if (i%100000==0 & i!=0)
+                    {
+                        A.szansa_krzyzowania += 5;
+                        
+                    }
+                if(i==50000 | i == 150000)
+                        A.szansa_mutacji += 1;
+                    if (i < 50000)
+                {
+                        A.policz_sume_min_max();
+                        A.oblicz_procenty();
+                        A.selekcja_ruletka();
+                        
+                }
+                else
+                {
+                        A.policz_sume_min_max();
+                        A.selekcja_turniej();
+
+                    }
+                A.mutacja();
+                    
+                A.ponowne_obliczanie_odleglosci();
+                    if(i%10000==0)
+                    {
+                        A.Najlepsza_Trasa();
+                        
+                    }
+            }
+                sw.WriteLine(A.Najlepsza_Trasa());
+                srednia.Add(A.najlepsza.ocena);
+            }
+            sw.WriteLine(srednia.Average() + " Srednia");
+            sw.WriteLine("---------------------------------------------------------");
+            sw.Close();
+            //A.pokaz_tablice_miast();
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    A.policz_sume_min_max();
+            //    A.oblicz_procenty();
+            //    A.mutacja2(0);
+            //    A.ponowne_obliczanie_odleglosci();
+            //}
+            //Console.WriteLine();
+            //A.pokaz_tablice_miast();
+
+            DateTime Db = DateTime.Now;
+            Console.WriteLine(Db.TimeOfDay - D.TimeOfDay);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Trasa.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication29
+{
+    class Trasa
+    {
+        public static Random liczbamiast = new Random();
+        private int[] tablicamiast = new int[0]; //przez jakie miasta przechodzi trasa [0,3,2,6,4]
+        public int[] pobierz_tablice_miast { get { return this.tablicamiast; } set { tablicamiast = value; } }
+        //zrobic ja stayczna
+        public static int[,] tablica = new int[0, 0];
+        public int ocena { get; set; }
+        public int wspolczynnik_procentowy { get; set; }
+        public static int wielkosc_tablicy;
+        static Trasa()
+        {
+           
+        }
+        public Trasa(int i)
+        {
+            tablicamiast = new int[i];
+            uzupełnienie_odległosci_miast();
+            losowanie_trasy();
+            ocena = this.obliczenie_odleglosci();
+        }
+        public Trasa(Trasa t1)
+        {
+            this.ocena = t1.ocena;
+            this.wspolczynnik_procentowy = t1.wspolczynnik_procentowy;
+            this.tablicamiast = t1.tablicamiast;
+        }
+
+        public void losowanie_trasy()
+        {
+            for (int j = 0; j < tablicamiast.Length; j++)
+            {
+                if (j == 0)
+                    tablicamiast[j] = Trasa.liczbamiast.Next(0, Trasa.tablica.GetLength(0));
+                else
+                {
+                    int liczba = 0;
+                    bool pombool = true;
+                    while (pombool)
+                    {
+                        liczba = Trasa.liczbamiast.Next(0, Trasa.tablica.GetLength(0));
+                        pombool = false;
+                        for (int z = 0; z < j; z++)
+                        {
+                            if (tablicamiast[z] == liczba)
+                            {
+                                pombool = true;
+                                break;
+                            }
+
+                        }
+                    }
+                    tablicamiast[j] = liczba;
+                }
+            }
+        }
+        public void pokaz_tablice_Miast()
+        {
+
+            foreach (int f in tablicamiast)
+                Console.Write(f + " ");
+            Console.WriteLine();
+        }
+        //tez ja zrobic statyczna
+        public static void uzupełnienie_odległosci_miast()
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\Mar\Desktop\berlin52.txt");
+            String tekst = sr.ReadLine();
+            tekst=tekst.Trim();
+            Trasa.wielkosc_tablicy = int.Parse(tekst);
+            Trasa.tablica = new int[Int16.Parse(tekst), Int16.Parse(tekst)];
+            String teskt2 = sr.ReadToEnd();
+            sr.Close();
+            String[] tekst3 = teskt2.Split(' ');
+            int pom = 0;
+            for (int i = 0; i < Trasa.tablica.GetLength(0); i++)
+            {
+                for (int y = 0; y <= i; y++)
+                {
+                    Trasa.tablica[i, y] = Int16.Parse(tekst3[pom]);
+                    Trasa.tablica[y, i] = Trasa.tablica[i, y];
+                    pom++;
+                }
+            }
+                
+            
+        }
+        
+        public int obliczenie_odleglosci()
+        {
+            int pierwsze = 0;
+            int drugie = 0;
+            int odleglosc = 0;
+            for (int i = 0; i + 1 < tablicamiast.Length; i++)
+            {
+                pierwsze = tablicamiast[i];
+                drugie = tablicamiast[i + 1];
+                odleglosc += Trasa.tablica[pierwsze, drugie];
+
+            }
+            odleglosc += Trasa.tablica[tablicamiast[0], tablicamiast[tablicamiast.Length - 1]];
+            return odleglosc;
+        }
+        public override string ToString()
+        {
+            string tekst="";
+            foreach (int x in tablicamiast)
+                tekst += x + "-";
+            tekst += " " + this.ocena;
+            return tekst;
+        }
+
+    }
+}
